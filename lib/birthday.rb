42995ce6babfc02d83b9616e09f96b0cb9415f0d
Christopher Dutcher <cadutcher@icloud.com>
2020-06-26 00:31:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     puts "Happy Birthday #{kid}! You are now #{age} years old!" if age < 12
+  else
+    puts "You are too old for this."
   end
 end
