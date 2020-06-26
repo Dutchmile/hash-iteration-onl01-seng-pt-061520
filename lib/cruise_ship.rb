e88a0468eeacd59b73bd77850e8a945548023b2f
Christopher Dutcher <cadutcher@icloud.com>
2020-06-26 00:20:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite.a &&
+    name.start_with?("A")
+    winner = name
+  end
 end
